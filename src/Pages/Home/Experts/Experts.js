e0657314f40aab5cf29b4374d9e0edfdb4b223ff
Rezaul909 <reza1381996@gmail.com>
2022-09-13 16:03:37 +0600

    added-experts

--- src/Pages/Home/Experts/Experts.js
+++ src/Pages/Home/Experts/Experts.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import './Experts.css'
+import expert1 from '../../../images/experts/expert-1.jpg';
+import expert2 from '../../../images/experts/expert-2.jpg';
+import expert3 from '../../../images/experts/expert-3.jpg';
+import expert4 from '../../../images/experts/expert-4.jpg';
+import expert5 from '../../../images/experts/expert-5.jpg';
+import expert6 from '../../../images/experts/expert-6.png';
+import Expert from '../Expert/Expert';
 
 const Experts = () => {
+    const experts = [
+        {name:"Jone Kobir", id:"1", img: expert1},
+        {name:"Jone Kholil", id:"2", img: expert2},
+        {name:"Jone Abraham", id:"3", img: expert3},
+        {name:"Jone Rohim", id:"4", img: expert4},
+        {name:"Jone Karim", id:"5", img: expert5},
+        {name:"Jone Khushi", id:"6", img: expert6},
+    ]
     return (
         <div>
+            <h1 className='text-center mt-5'>Our Experts</h1>
+            <div className="experts-container container mt-5">
+
+                {
+                    experts.map(expert=> <Expert
+                        key={expert.id}
+                        expert={expert}
+                    ></Expert> )
+                }
             
+            </div>
         </div>
     );
 };
