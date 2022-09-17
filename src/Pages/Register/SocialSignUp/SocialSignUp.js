502ac2e0013d520ffc413f1be9a34c320c68e0ae
Rezaul909 <reza1381996@gmail.com>
2022-09-18 04:57:07 +0600

    Google SignUp btn added

--- src/Pages/Register/SocialSignUp/SocialSignUp.js
+++ src/Pages/Register/SocialSignUp/SocialSignUp.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { FaGoogle, FaFacebookSquare, FaGithub } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
 
 const SocialSignUp = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+
+    if(user){
+        navigate('/home')
+    }
+
+    let errorMassage;
+    if (error) {
+
+        errorMassage =  <div>
+            <p className="text-danger">Error: {error.message}</p>
+          </div>
+      }
+
   return (
     <div className="text-center">
+        {errorMassage}
       <h3>or</h3>
       <div className="d-flex align-items-center justify-content-evenly">
         <div style={{height:'3px'}} className="bg-secondary border w-25"></div>
@@ -12,8 +31,8 @@ const SocialSignUp = () => {
       </div>
 
       <div className="mt-3 d-flex align-items-center justify-content-evenly">
-        <button className="btn btn-info fs-5 my-2 px-5">
-            <FaGoogle size='1.5em' color="green"></FaGoogle> Google
+        <button onClick={()=> signInWithGoogle()} className="btn btn-info fs-5 my-2 px-5">
+            <FaGoogle size='1.5em' color="blue"></FaGoogle> Google
         </button>
         <button className="btn btn-info fs-5 my-2 px-5">
             <FaFacebookSquare size='1.5em' color="blue"></FaFacebookSquare> Facebook
